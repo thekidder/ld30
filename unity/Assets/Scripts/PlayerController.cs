3b6e8a7e4063aa183f608012007b6aaa2a03b11f
Adam Kidder <thekidder@gmail.com>
2014-08-23 20:23:09 -0700

    movement clipping

--- unity/Assets/Scripts/PlayerController.cs
+++ unity/Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 	public const float SIZE = 32f;
+	
+	public NoiseField noise;
 
 	private float lastMovementTime = 0f;
 	// Use this for initialization
@@ -12,10 +14,10 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if(Time.time - lastMovementTime > 0.02f) {
-			transform.localPosition = transform.localPosition + new Vector3(
-				Input.GetAxisRaw("Horizontal") * SIZE,
-				Input.GetAxisRaw("Vertical") * SIZE,
-				0f);
+			transform.localPosition = new Vector3(
+				Mathf.Clamp(transform.localPosition.x + Input.GetAxisRaw("Horizontal") * SIZE, 0f, SIZE * (noise.width - 1)),
+				Mathf.Clamp(transform.localPosition.y + Input.GetAxisRaw("Vertical") * SIZE, 0f, SIZE * (noise.height - 1)),
+				transform.localPosition.z + 0f);
 			lastMovementTime = Time.time;
 		}
 	}
