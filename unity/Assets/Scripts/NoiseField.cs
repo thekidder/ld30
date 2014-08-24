c2055c10f97ebfac76db8f1b96d7454d6dc3a17a
Adam Kidder <thekidder@gmail.com>
2014-08-23 20:09:25 -0700

    feature parity with dart

---
+++ unity/Assets/Scripts/NoiseField.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NoiseField : MonoBehaviour {
+	public GameObject player;
+	
+	public int width;
+	public int height;
+	
+	public float noiseFrequency;
+	
+	private float lastNoiseGen = 0f;
+
+	// Use this for initialization
+	void Start () {
+		GameObject pixel = (GameObject)Resources.Load("Prefabs/Pixel");
+		
+		for(int i = 0; i < width; ++i) {
+			for(int j = 0; j < height; ++j) {
+				GameObject go = (GameObject)GameObject.Instantiate(pixel);
+				go.name = string.Format("noise_{0}_{1}", i, j);
+				go.transform.parent = this.transform;
+				go.transform.localPosition = new Vector3(PlayerController.SIZE * i, PlayerController.SIZE * j, 0);
+            }
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for(int i = 0; i < width; ++i) {
+			for(int j = 0; j < height; ++j) {
+				GameObject point = gameObject.transform.GetChild(j + i * height).gameObject;
+				Vector2 dist = transform.position + new Vector3(i * PlayerController.SIZE, j * PlayerController.SIZE, 0f) - player.transform.position;
+                float alpha = Mathf.Clamp(dist.sqrMagnitude, 0f, 102400f) / 102400f;
+				Color c = point.GetComponent<SpriteRenderer>().color;
+				c.a = alpha;
+				point.GetComponent<SpriteRenderer>().color = c;
+			}
+		}
+	
+		if(Time.time - lastNoiseGen < noiseFrequency) {
+			return;
+		}
+		lastNoiseGen = Time.time;
+		for(int i = 0; i < width; ++i) {
+			for(int j = 0; j < height; ++j) {
+				GameObject point = gameObject.transform.GetChild(j + i * height).gameObject;
+				float noise = Random.Range(0.4f, 0.6f);
+				point.GetComponent<SpriteRenderer>().color = new Color(noise, noise, noise + 0.05f, point.GetComponent<SpriteRenderer>().color.a);
+            }
+		}
+	}
+}
---
+++ unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public const float SIZE = 32f;
+
+	private float lastMovementTime = 0f;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time - lastMovementTime > 0.02f) {
+			transform.localPosition = transform.localPosition + new Vector3(
+				Input.GetAxisRaw("Horizontal") * SIZE,
+				Input.GetAxisRaw("Vertical") * SIZE,
+				0f);
+			lastMovementTime = Time.time;
+		}
+	}
+}
